28ddeb117642f9297d235e843ea92484bc6d3dc1
Ashutosh Jagtap <ashujagtap333@gmail.com>
2020-07-17 20:53:26 +0530

    changes in folder structure

--- src/components/app/app.module.ts
+++ src/components/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ProductsComponent } from './products/products.component';
+import { AppComponent } from 'src/components/app/app.component';
+import { HeaderComponent } from 'src/components//header/header.component';
+import { ProductsComponent } from 'src/components//products/products.component';
 
 @NgModule({
   declarations: [
