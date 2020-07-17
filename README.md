f50cb5cc2fc4c9bdbfac25407c3ca7debed072fd
Ashutosh Jagtap <ashujagtap333@gmail.com>
2020-07-17 13:06:32 +0530

    Angular initial setup

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # OnlineMobileShop
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.11.
+
---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'OnlineMobileShop'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('OnlineMobileShop');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('OnlineMobileShop app is running!');
+  });
+});
