c14986144a9cc083979126de60eee7c2da500bdb
Ashutosh Jagtap <ashujagtap333@gmail.com>
2020-07-17 22:05:21 +0530

    created Product Model for showing products.

--- src/components/products/products.component.ts
+++ src/components/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/models/product';
 
 @Component({
   selector: 'app-products',
@@ -7,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  productList: Product[];
+
+  constructor() {
+    this.productList = [
+      {
+        id: '61a550da-10ad-42e9-a783-3d4f0b7419d2',
+        name: 'iPhone 11',
+        brand: 'iPhone',
+        description: 'All-new dual-camera system with Ultra Wide and Night mode. All-day battery. Six new colours. And the A13 Bionic, our fastest chip ever.',
+        imgUrl: 'https://cdn.vox-cdn.com/thumbor/Xt4Wo_s37BD4tceJkxVRYjKJV5w=/0x0:1920x1280/920x613/filters:focal(807x487:1113x793):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/61151465/jbareham_171101_2099_A_0088_02.0.jpg',
+        price: 10
+      }
+    ];
+  }
 
   ngOnInit(): void {
   }
---
+++ src/models/product.ts
@@ -0,0 +1,8 @@
+export class Product {
+    id: string;
+    name: string;
+    brand: string;
+    price: number;
+    description: string;
+    imgUrl: string;
+}
