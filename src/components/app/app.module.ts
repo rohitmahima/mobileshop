d4f13d42107826e0fe071cc06052f7c6d7e3cc37
Ashutosh Jagtap <ashujagtap333@gmail.com>
2020-07-19 20:59:47 +0530

    Implemented Routing.

--- src/components/app/app.module.ts
+++ src/components/app/app.module.ts
@@ -2,11 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from '../app/app.component';
 import { HeaderComponent } from '../header/header.component';
 import { ProductsComponent } from '../products/products.component';
 import { ProductAddComponent } from '../product-add/product-add.component';
+
+const appRoutes: Routes = [
+  { path: '', component: ProductsComponent },
+  { path: 'addProduct', component: ProductAddComponent }
+];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,7 +24,8 @@ import { ProductAddComponent } from '../product-add/product-add.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
